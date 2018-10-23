96c70bcbecf178223efcf8768e16bd3592246f84
juliafsouzag <juliafsouzag@gamil.com>
2018-10-22 23:04:05 -0300

    Alterações na funcao remover, inserir e main

--- BinarySearchTree/src/Main.java
+++ BinarySearchTree/src/Main.java
@@ -69,12 +69,13 @@ public class Main {
 
 				case "BUSCA": {
 					Integer value = Integer.valueOf(comando[1]);
-					boolean found = tree.search(value);
-					if (found) {
-						System.out.println("O valor " + value + " encontra-se na árvore");
-					} else {
-						System.out.println("O valor " + value + " não foi encontrado!");
-					}
+			    	boolean found = tree.search(value);
+			    		if(found) {
+			    			System.out.println("O valor " + value + " encontra-se na árvore!" );
+			    		}
+			    		else {
+			    			System.out.println("O valor " + value + ", pois ele não se encontra na árvore!");
+			    		}
 				}
 					break;
 
@@ -94,6 +95,10 @@ public class Main {
 					break;
 
 				case "IMPRIMA": {
+
+                    //Só para verificar se a checagem está correta
+                    System.out.println("O numero de filhos da raiz a esquerda é: " + tree.getRoot().getCountLeftNodes() + " e a direita é: " + tree.getRoot().getCountRigthNodes());
+                    System.out.println("O valor da raiz é " + tree.getRoot().getValue());
 
 				}
 					break;
